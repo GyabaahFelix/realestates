bf7e19e04dd8a5ca6da83d5f096aa9f428a25b29
GyabaahFelix <fgyabaah003@st.ug.edu.gh>
2024-08-24 22:58:00 +0000

    Changed value to Value

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Hero from "./components/Hero/Hero";
 import './App.css'
 import Companies from "./components/Companies/Companies";
 import Residencies from "./components/Residencies/Residencies";
-import Value from "./components/Value/value";
+import Value from "./components/Value/Value";
 import Contact from "./components/Contact/Contact";
 import GetStarted from "./components/GetStarted/GetStarted";
 import Footer from "./components/Footer/Footer";
